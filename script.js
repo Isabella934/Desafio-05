257173a8b2ec40226c4c1d5c3650fd1e898f0825
Isabella934 <isabella.lessa27@gmail.com>
2022-07-08 17:13:04 -0300

    desafio 05 de lógica

---
+++ script.js
@@ -0,0 +1,20 @@
+let frutas = ['banana', 'mamão', 'uva', 'morango', 'caqui'];
+
+console.log(frutas)
+
+frutas.unshift('melancia')
+console.log(frutas)
+
+frutas.pop()
+console.log(frutas)
+
+frutas.push('amora', 'melão')
+console.log(frutas)
+
+frutas.shift()
+console.log(frutas)
+
+let numbers = [7,5,6,3,8,9,2,1,4]
+
+numbers.sort()
+console.log(numbers)
